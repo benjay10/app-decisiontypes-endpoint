17d6c554764033c9141da4557e24a1ae7a4d17ab
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-05-27 18:26:33 +0200

    Initial commit

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -9,15 +9,15 @@ defmodule Dispatcher do
   @json %{ accept: %{ json: true } }
   @html %{ accept: %{ html: true } }
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
-  # Run `docker-compose restart dispatcher` after updating
-  # this file.
+  # Forward all JSON requests on /api to the mu-cl-resource
+  get "/api/*path", @json do
+    Proxy.forward conn, path, "http://resources/"
+  end
+
+  # Forward all JSON requests on /sparql to the triplestore directly
+  match "/sparql/*path", _ do
+    Proxy.forward conn, [], "http://database:8890/sparql"
+  end
 
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
